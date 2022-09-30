cb5cb40004d21408b6b3bc8abc87133eafef591d
sayaka tamura <goooodnight@yahoo.co.jp>
2022-09-30 12:22:54 +0900

    letter_opener_webの設定とdevise userモデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :tasks
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'tasks#index'
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
