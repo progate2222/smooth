967bf061b8172296ab9d88b0be1a9de0d93e16e7
sayaka tamura <goooodnight@yahoo.co.jp>
2022-10-04 11:42:38 +0900

    カラム名変更　user_name→name

---
+++ db/migrate/20221004023538_rename_username_column_to_name.rb
@@ -0,0 +1,5 @@
+class RenameUsernameColumnToName < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :users, :user_name, :name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_30_100545) do
+ActiveRecord::Schema.define(version: 2022_10_04_023538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2022_09_30_100545) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "user_name"
+    t.string "name"
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
