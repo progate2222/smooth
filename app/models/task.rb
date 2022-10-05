5515ea954cd3cc6782578084a562336b48d0c8ac
sayaka tamura <goooodnight@yahoo.co.jp>
2022-10-05 10:26:35 +0900

    重要度のenum設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+    enum importance: { "高": 1, "中": 2, "低": 3 }
     belongs_to :user
     has_many :requests, dependent: :destroy
     accepts_nested_attributes_for :requests, allow_destroy: true, reject_if: :all_blank
