ec6e13904b8f470e3cfcaa8460b5effd792c3969
sayaka tamura <goooodnight@yahoo.co.jp>
2022-10-21 12:31:15 +0900

    管理者権限のテストの実装

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -48,27 +48,207 @@ RSpec.describe 'ユーザー管理機能', type: :system do
         end
     end
 
-    # describe '管理者権限のテスト' do
-    #     before do
-    #         visit new_user_session_path
-    #         fill_in "user_email", with: 'user_admin@mail.com'
-    #         fill_in "user_password", with: 'user_admin'
-    #         find('#login').click
-    #         visit rails_admin_path
-    #     end
+    describe '管理者権限のテスト' do
+        before do
+            visit new_user_session_path
+            fill_in "user_email", with: 'user_admin@mail.com'
+            fill_in "user_password", with: 'user_admin'
+            find('#login').click
+            visit rails_admin_path
+        end
 
-    #     context '管理ユーザは' do
-    #         it 'ユーザーを新規作製できる(Create)' do
-    #             click_on(all(".nav-link")[4])
-    #             click_on('新規登録')
-    #             fill_in "user_email", with: 'test@mail.com'
-    #             fill_in "user_password", with: '111111'
-    #             fill_in "user_password_confirmation", with: '111111'
-    #             fill_in "user_name", with: 'test'
-    #             click_on('保存')
-    #             expect(page).to have_content 'ユーザーの作成に成功しました'
-    #         end
-    #     end
-    # end
+        context '管理ユーザは' do
+            it 'ユーザーを新規作製できる(Create)' do
+                sleep 1.0
+                page.all(".nav-link")[8].click
+                sleep 1.0
+                page.all(".nav-link")[10].click
+                fill_in "user_email", with: 'test@mail.com'
+                fill_in "user_password", with: '111111'
+                fill_in "user_password_confirmation", with: '111111'
+                fill_in "user_name", with: 'test'
+                click_on('保存')
+                expect(page).to have_content 'ユーザーの作成に成功しました'
+            end
+            it 'ユーザーの詳細を確認できる(Read)' do
+                sleep 1.0
+                page.all(".nav-link")[8].click
+                sleep 1.0
+                page.all(".show_member_link").last.click
+                expect(page).to have_content "ユーザー 'user_general'の詳細"
+            end
+            it 'ユーザーを編集できる(Update)' do
+                sleep 1.0
+                page.all(".nav-link")[8].click
+                sleep 1.0
+                page.all(".fa-pencil-alt")[1].click
+                fill_in "user_name", with: 'Update_user'
+                click_on('保存')
+                expect(page).to have_content 'ユーザーの更新に成功しました'
+            end
+            it 'ユーザーを削除できる(Destroy)' do
+                sleep 1.0
+                page.all(".nav-link")[8].click
+                sleep 1.0
+                page.all(".fa-times").last.click
+                click_on('実行する')
+                expect(page).to have_content 'ユーザーの削除に成功しました'
+            end
+        end
+
+        context '管理ユーザは' do
+            it 'チームを新規作製できる(Create)' do
+                sleep 1.0
+                page.all(".nav-link")[5].click
+                sleep 1.0
+                page.all(".nav-link")[10].click
+                fill_in "team_name", with: 'テストチーム'
+                click_on('保存')
+                expect(page).to have_content 'Teamの作成に成功しました'
+            end
+            it 'チームの詳細を確認できる(Read)' do
+                sleep 1.0
+                page.all(".nav-link")[5].click
+                sleep 1.0
+                page.all(".nav-link")[10].click
+                fill_in "team_name", with: 'テストチーム'
+                click_on('保存')
+                sleep 1.0
+                page.all(".nav-link")[5].click
+                sleep 1.0
+                page.all(".fa-info-circle").last.click
+                expect(page).to have_content "Team 'テストチーム'の詳細"
+            end
+            it 'チームを編集できる(Update)' do
+                sleep 1.0
+                page.all(".nav-link")[5].click
+                sleep 1.0
+                page.all(".nav-link")[10].click
+                fill_in "team_name", with: 'テストチーム'
+                click_on('保存')
+                sleep 1.0
+                page.all(".fa-pencil-alt").last.click
+                fill_in "team_name", with: 'Update_team'
+                click_on('保存')
+                expect(page).to have_content 'Teamの更新に成功しました'
+            end
+            it 'チームを削除できる(Destroy)' do
+                sleep 1.0
+                page.all(".nav-link")[5].click
+                sleep 1.0
+                page.all(".nav-link")[10].click
+                fill_in "team_name", with: 'テストチーム'
+                click_on('保存')
+                sleep 1.0
+                page.all(".fa-times").last.click
+                click_on('実行する')
+                expect(page).to have_content 'Teamの削除に成功しました'
+            end
+        end
+
+        context '管理ユーザは' do
+            it 'チームメンバーを新規作製できる(Create)' do
+                sleep 1.0
+                page.all(".nav-link")[6].click
+                sleep 1.0
+                page.all(".nav-link")[10].click
+                click_on('新規ユーザー追加')
+                sleep 1.0
+                fill_in "user_email", with: 'test@mail.com'
+                fill_in "user_password", with: '111111'
+                fill_in "user_password_confirmation", with: '111111'
+                fill_in "user_name", with: 'test'
+                page.all(".save-action").last.click
+                sleep 1.0
+                click_on('新規Team追加')
+                fill_in "team_name", with: 'テストチーム'
+                sleep 1.0
+                page.all(".save-action").last.click
+                sleep 1.0
+                click_on('保存')
+                expect(page).to have_content 'Team memberの作成に成功しました'
+            end
+            it 'チームメンバーの詳細を確認できる(Read)' do
+                sleep 1.0
+                page.all(".nav-link")[6].click
+                sleep 1.0
+                page.all(".nav-link")[10].click
+                click_on('新規ユーザー追加')
+                sleep 1.0
+                fill_in "user_email", with: 'test@mail.com'
+                fill_in "user_password", with: '111111'
+                fill_in "user_password_confirmation", with: '111111'
+                fill_in "user_name", with: 'test'
+                page.all(".save-action").last.click
+                sleep 1.0
+                click_on('新規Team追加')
+                fill_in "team_name", with: 'テストチーム'
+                sleep 1.0
+                page.all(".save-action").last.click
+                sleep 1.0
+                click_on('保存')
+                sleep 1.0
+                page.all(".nav-link")[6].click
+                sleep 1.0
+                page.all(".fa-info-circle").first.click
+                expect(page).to have_content "Team member 'TeamMember #"
+                expect(page).to have_content "の詳細"
+            end
+            it 'チームメンバーを編集できる(Update)' do
+                sleep 1.0
+                page.all(".nav-link")[6].click
+                sleep 1.0
+                page.all(".nav-link")[10].click
+                click_on('新規ユーザー追加')
+                sleep 1.0
+                fill_in "user_email", with: 'test@mail.com'
+                fill_in "user_password", with: '111111'
+                fill_in "user_password_confirmation", with: '111111'
+                fill_in "user_name", with: 'test'
+                page.all(".save-action").last.click
+                sleep 1.0
+                click_on('新規Team追加')
+                fill_in "team_name", with: 'テストチーム'
+                sleep 1.0
+                page.all(".save-action").last.click
+                sleep 1.0
+                click_on('保存')
+                sleep 1.0
+                page.all(".fa-pencil-alt").last.click
+                click_on('新規Team追加')
+                fill_in "team_name", with: 'テストチーム2'
+                sleep 1.0
+                page.all(".save-action").last.click
+                sleep 1.0
+                click_on('保存')
+                expect(page).to have_content 'Team memberの更新に成功しました'
+            end
+            it 'チームメンバーを削除できる(Destroy)' do
+                sleep 1.0
+                page.all(".nav-link")[6].click
+                sleep 1.0
+                page.all(".nav-link")[10].click
+                click_on('新規ユーザー追加')
+                sleep 1.0
+                fill_in "user_email", with: 'test@mail.com'
+                fill_in "user_password", with: '111111'
+                fill_in "user_password_confirmation", with: '111111'
+                fill_in "user_name", with: 'test'
+                page.all(".save-action").last.click
+                sleep 1.0
+                click_on('新規Team追加')
+                fill_in "team_name", with: 'テストチーム'
+                sleep 1.0
+                page.all(".save-action").last.click
+                sleep 1.0
+                click_on('保存')
+                sleep 1.0
+                page.all(".fa-times").last.click
+                click_on('実行する')
+                expect(page).to have_content 'Team memberの削除に成功しました'
+            end
+        end
+
+    end
 
 end
